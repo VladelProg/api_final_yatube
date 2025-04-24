96170deea53350bf7d83ee7d0b9e91c71a3d64e8
Владислав Елькин <vladel_2003@bk.ru>
2025-04-24 20:22:51 +0700

    5 review

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
     description = models.TextField()
 
     def __str__(self):
@@ -21,9 +21,8 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
     group = models.ForeignKey(
-        Group, on_delete=models.CASCADE,
-        related_name="posts", blank=True, null=True
-    )
+        Group, on_delete=models.CASCADE, related_name='posts',
+        null=True, blank=True)
 
     def __str__(self):
         return self.text
@@ -41,8 +40,10 @@ class Comment(models.Model):
 
 class Follow(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='follower'
-    )
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower')
     following = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='following'
-    )
+        User,
+        on_delete=models.CASCADE,
+        related_name='following')
